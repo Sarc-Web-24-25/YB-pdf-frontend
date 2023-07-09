5183b7a450c168be0b9dae4af00c8c1f81c91a70
akash-banger2 <akashbanger@troopapp.in>
2023-07-09 23:12:58 +0530

    kind of completed, logic left

--- src/Pages/PDFGenerator.jsx
+++ src/Pages/PDFGenerator.jsx
@@ -1,17 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Page, Text, View, Image, Document, StyleSheet } from '@react-pdf/renderer';
 import { styles } from './styles/PDFStyles.js';
-import background from './assets/background.jpg'
+import background from './assets/background.png'
+import csvData from './CSVdata/Posts.csv'
+
+
 
 const PDFGenerator = () => {
+
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+
+    fetch(csvData) // Replace with the appropriate URL or file path if needed
+      .then(response => response.text())
+      .then(csvText => {
+        // Pass the CSV text to the parseCSV function
+        const rows = csvText.split('\n');
+        const headers = rows[0].split(','); // Assuming the first row contains headers
+        const results = [];
+
+        for (let i = 1; i < rows.length; i++) {
+          const row = rows[i].split(',');
+          const rowData = {};
+
+          for (let j = 0; j < headers.length; j++) {
+            rowData[headers[j]] = row[j];
+          }
+
+          results.push(rowData);
+        }
+
+
+        setData(results);
+      })
+      .catch(error => {
+        console.error('Error reading CSV file:', error);
+      });
+
+
+  }, []);
+
+
+  const MAX_POSTS_PER_PAGE = 4;
+
+  const totalPosts = data.length;
+  const totalPages = Math.ceil(totalPosts / MAX_POSTS_PER_PAGE);
+
+  const getPageData = (page) => {
+    const start = (page - 1) * MAX_POSTS_PER_PAGE;
+    const end = start + MAX_POSTS_PER_PAGE;
+    return data.slice(start, end);
+  };
+
+
+
   return (
     <Document>
-      <Page size="A4" style={styles.page} >
-        <View style={styles.section}>
-          <Image src={background} style={styles.backgroundImg} />
-          <Text style={styles.text}>First Page</Text>
-        </View>
-      </Page>
+      {Array.from(Array(totalPages).keys()).map((page) => {
+        
+        <Page size="A4" style={styles.page} >
+          <View style={styles.section}>
+            <Image src={background} style={styles.backgroundImg} />
+            <View style={styles.container}>
+
+              <View>
+                <View style={styles.postContainer}>
+                  <View styles={styles.imageContainer}>
+                    <Image src={background} style={styles.profilePicLeft} />
+                    <Text style={styles.profileTextLeft}>John Doe</Text>
+                  </View>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.content}>{"post.content"}</Text>
+                  </View>
+                </View>
+
+                <View style={styles.postContainer}>
+                  <View style={styles.textContainerRight}>
+                    <Text style={styles.content}>{"post.content"}</Text>
+                  </View>
+                  <View styles={styles.imageContainerRight}>
+                    <Image src={background} style={styles.profilePicRight} />
+                    <Text style={styles.profileTextRight}>John Doe</Text>
+                  </View>
+                </View>
+              </View>
+
+            </View>
+          </View>
+        </Page>
+      })}
     </Document>
   );
 };
--- src/Pages/styles/PDFStyles.js
+++ src/Pages/styles/PDFStyles.js
@@ -7,19 +7,91 @@ export const styles = StyleSheet.create({
   },
   backgroundImg: {
     position: 'absolute',
-    left: 0,
     top: 0,
+    left: 0,
     width: '100%',
     height: '100%',
   },
   section: {
     position: "relative",
     zIndex: 1,
-    margin: 10,
-    padding: 10,
     flexGrow: 1,
   },
-  text: {
-    fontFamily: "Helvetica"
+
+  imageContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    transform: 'rotate(-10deg)',
+  },
+
+  imageContainerRight: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    transform: 'rotate(10deg)',
+  },
+
+  profileTextLeft:{
+    margin: 5,
+    marginLeft: 25,
+    transform: 'rotate(-10deg)',
+  },
+
+  profileTextRight:{
+    margin: 5,
+    marginRight: 25,
+    transform: 'rotate(10deg)',
+  },
+
+  profilePicLeft: {
+    width: "1.7in",
+    height: "1.7in",
+    transform: 'rotate(-10deg)',
+  },
+
+  profilePicRight: {
+    width: "1.7in",
+    height: "1.7in",
+    transform: 'rotate(10deg)',
+  },
+
+  container: {
+    paddingTop: "12%",
+    paddingBottom: "12%",
+    paddingLeft: "10%",
+    paddingRight: "10%",
+  },
+
+  postContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+
+  textContainer: {
+    width: "70%",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+
+  textContainerRight: {
+    width: "70%",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+
+  content: {
+    fontSize: 12,
   }
 });
