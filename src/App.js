7212d082758946a9921d23f033d7adb6d225639a
Ashwanidubeyiitb <dubeyash2004@gmail.com>
2024-06-24 05:42:53 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import {
 import PDFGenerator from "./Pages/PDFGenerator";
 
 const App = () => {
-  const idList = [66]; // Replace this with your actual list of ids
+  const idList = [495 ]; // Replace this with your actual list of ids
 
-  const ids = [[
-    // 205, 167, 891, 1578, 642, 23, 772, 339, 976, 1719, 2295, 515, 2751, 1288, 831, 60, 1323, 405, 47, 63, 80, 326, 70, 2754, 956, 391, 69, 1073, 3704, 626, 823, 2279, 3291, 2018, 2188, 235, 1256, 2711, 940, 130, 87, 749, 279, 152, 49, 2779, 1598, 43, 717, 2760, 76, 1002, 1760, 1822, 3715, 213, 447, 3657, 4222, 84, 1891, 1375, 5065, 991
+  const ids = [[ 
+    23,11,51,48,7,42,52,10
   ]];
   const downloadAnchorRef = useRef(null);
   const [readyToDownload, setReadyToDownload] = useState(false);
